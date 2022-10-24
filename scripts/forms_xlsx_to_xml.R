4c53e55dfae5d2ada95aa93b1dfe17d8819680e3
cw-data <charles.a.wainright@gmail.com>
2022-10-24 09:14:54 -0500

    added nodes "database", "source-app", "rec-number"

--- scripts/forms_xlsx_to_xml.R
+++ scripts/forms_xlsx_to_xml.R
@@ -32,28 +32,41 @@ data <-
                 stable_url = `Stable URL or DOI`) %>%
   dplyr::filter(is.na(title) == FALSE) # just grab one row that's not missing any values
 
-######################################
-### make and print a basic xml tree
+data2 <- data.frame(reference_type = "Journal", # make a fake record, so we can test iterating over multiple records
+                    title = "Important science",
+                    authors = "Person Name",
+                    year = "1999",
+                    files_attached = "https://usdagcc-my.sharepoint.com/personal/margaret_woodbridge_usda_gov/Documents/Apps",
+                    where = "a place",
+                    description = "this is a text description",
+                    cover_type = "some cover type",
+                    stable_url = "google.com")
 
-xml <- xmlTree()
-# names(xml) # list out the functions available in xmlTree()
-xml$addNode("xml", close=FALSE) # adds an opening <xml> tag and leaves it open until we close it
-xml$addNode("records", close=FALSE) # adds an opening <records> tag and leaves it open until we close it 
-xml$addNode(name = "database", # how to add nodes with attributes (or namespaces) # from ?xmlTree
-             attrs = c(
-               name = "My Test library.enl",
-               path = "C:\\Users\\mwroberts\\Documents\\My Test library.enl"
-             ),
-             "My Test library.enl")
-# <source-app name="EndNote" version="20.4">EndNote</source-app>
-xml$addNode(name = "source-app",
-             attrs = c(name = "EndNote",
-                       version = "20.4"),
-             
-             "EndNote")
-for (i in 1:nrow(data)) {
+data <- rbind(data, data2) # add the fake record to data
+######################################
+### build the realistic xml tree from xlsx data one node at a time
+# make the nodes of our xml match the xml from "data/enl_xml_schema.txt"
+
+xml <- xmlTree() # start with an empty xml tree
+xml$addNode("xml", close=FALSE) # add top-level nodes. adds an opening <xml> tag and leaves it open until we close it
+xml$addNode("records", close=FALSE) # another top-levle node. adds an opening <records> tag and leaves it open until we close it 
+for (i in 1:nrow(data)) { # for each row in our dataframe, we need to add these tags:
   xml$addTag("record", close=FALSE) # each row from data goes into its own <record> tag, leave the tag open until we close it
-  for (j in colnames(data)) {
+  xml$addNode(name = "database", # how to add nodes with attributes (or namespaces) # from ?xmlTree
+              attrs = c(
+                name = "My Test library.enl",
+                path = "C:\\Users\\mwroberts\\Documents\\My Test library.enl"
+              ),
+              "My Test library.enl")
+  # <source-app name="EndNote" version="20.4">EndNote</source-app>
+  xml$addNode(name = "source-app",
+              attrs = c(name = "EndNote",
+                        version = "20.4"),
+              
+              "EndNote")
+  xml$addNode(name = "rec-number",
+              i) # I don't know how EndNote assigns 'rec-number' so, for now, I'm guessing it's just index. May need to write logic to start increment at nrow(existing number of rows) + 1. tbd
+  for (j in colnames(data)) { # we need to add tags at the intersection of row and column
     xml$addTag(j, data[i, j]) # for each column in data, the value in [row i, column j] goes into a tag named <colname j>
     }
   xml$closeTag() # add a closing tag to each: </record>
@@ -62,16 +75,12 @@ xml$closeTag() # close the records tag: </records>
 xml$closeTag() # close the xml tag: </xml>
 cat(saveXML(xml, prefix='<?xml version="1.0" encoding="UTF-8"?>')) # print to console the xml we just looped to create
 
-# known problems with this basic xml tree:
-### 1. Despite <xml> being the root tag in endnote's xml export, the schema rejects a root <xml> tag.
-### need to test a real endnote import to see if wants the root <xml> tag or not
-### 2. 
 
+# known problems with this xml tree, based on "scripts/xml_vs_schema_validation.R":
 
-
-node <- xmlNode("foo", attrs=c(a="1", b="my name"))
-
-xmlGetAttr(node, "a")
-xmlGetAttr(node, "doesn't exist", "My own default value")
-
-xmlGetAttr(node, "b", "Just in case")
+### 1. Despite <xml> being the root node in endnote's xml export, validating EndNote's exprot XML against its own schema fails at the root <xml> node,
+### <xml> tag is not present in "resources/RSXML.xsd" # resolved by removing tags <xml></xml> from "data/real_enl_xml.xml"
+### need to test a real endnote import to see if wants the root <xml> tag or not.
+### 2. Same thing as #1. <foreign-keys> tag is not present in "resources/RSXML.xsd" # resolved by removing tags and data <foreign-keys>data</foreign-keys> from "data/real_enl_xml.xml"
+### 3. Same thingas #1. got <pdf-urls> but expected <image-urls>. # resolved by replacing tags <pdf-urls></pdf-urls> with <image-urls></image-urls> in "data/real_enl_xml.xml"
+### with those changes, "data/real_enl_xml.xml" validates as TRUE "resources/RSXML.xsd"
