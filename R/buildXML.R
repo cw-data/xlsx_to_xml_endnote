3379326f3e66352a371e0b0da1ff14824a2de89c
cw-data <charles.a.wainright@gmail.com>
2023-01-08 15:23:09 -0500

    initial commit

---
+++ R/buildXML.R
@@ -0,0 +1,22 @@
+
+
+
+#----- load external libraries
+library(readxl)
+library(dplyr)
+library(data.table)
+library(xml2)
+
+#----- load project functions
+source("R/book.R")
+# source("R/fsPub.R")
+# source("R/journal.R")
+# source("R/govDoc.R")
+# source("R/thesis.R")
+# source("R/map.R")
+# source("R/bookSection.R")
+# source("R/confPaper.R")
+# source("R/photo.R")
+# source("R/website.R")
+# source("R/newspaper.R")
+# source("R/confProceed.R")
